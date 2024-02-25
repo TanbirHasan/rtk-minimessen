a9618af926cc246b09d4c7463c86fdc9daee7d8b
TanbirHasan <mdtanbir576@gmail.com>
2024-02-25 12:56:25 +0600

    modal state update

--- src/components/inbox/Modal.js
+++ src/components/inbox/Modal.js
@@ -1,4 +1,8 @@
+import { useState } from "react";
+
 export default function Modal({ open, control }) {
+    const [to,setTo] = useState('')
+    const [message,setMessage] = useState('')
     return (
         open && (
             <>
@@ -10,8 +14,8 @@ export default function Modal({ open, control }) {
                     <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
                         Send message
                     </h2>
-                    <form className="mt-8 space-y-6" action="#" method="POST">
-                        <input type="hidden" name="remember" value="true" />
+                    <form className="mt-8 space-y-6">
+                        
                         <div className="rounded-md shadow-sm -space-y-px">
                             <div>
                                 <label htmlFor="to" className="sr-only">
@@ -21,6 +25,7 @@ export default function Modal({ open, control }) {
                                     id="to"
                                     name="to"
                                     type="to"
+                                    value={to}
                                     required
                                     className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-violet-500 focus:border-violet-500 focus:z-10 sm:text-sm"
                                     placeholder="Send to"
@@ -34,6 +39,8 @@ export default function Modal({ open, control }) {
                                     id="message"
                                     name="message"
                                     type="message"
+                                    value={message}
+                                    onChange={(e) => setMessage(e.target.message)}
                                     required
                                     className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-violet-500 focus:border-violet-500 focus:z-10 sm:text-sm"
                                     placeholder="Message"
